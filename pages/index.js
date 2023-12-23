1efcb4e1d3b5f114a5b9888125b378f6ce0b4143
Prakhar Sachan <91385023+PrakharSachan5342@users.noreply.github.com>
2023-12-24 02:36:22 +0530

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import styles from "./index.module.css";
+
+const Desktop1 = () => {
+  return (
+    <div className={styles.desktop1}>
+      <div className={styles.desktop1Child} />
+      <div className={styles.edify}>Edify</div>
+    </div>
+  );
+};
+
+export default Desktop1;
